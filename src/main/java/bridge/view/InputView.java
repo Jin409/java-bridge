d50266ca8b312e77824a64b3455804c34f82c00b
Jin409 <sualng123@gmail.com>
2022-11-17 22:57:10 +0900

    refactor(InputView): 에러 메시지에 상수를 사용하지 않는 것으로 수정
    - 에러 메시지에 상수를 사용하게 되면, 문자열 더하기에 의해 비효율이 발생할 것을 우려해 상수를 사용하지 않는 것으로 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,17 +1,12 @@
 package bridge.view;
 
-import static bridge.constant.BridgeGameConstant.DOWN_SIGN;
-import static bridge.constant.BridgeGameConstant.QUIT_SIGN;
-import static bridge.constant.BridgeGameConstant.RETRY_SIGN;
-import static bridge.constant.BridgeGameConstant.UP_SIGN;
-
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String READ_BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
-    private static final String READ_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: " + UP_SIGN + ", 아래: " + DOWN_SIGN + ")";
+    private static final String READ_MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
     private static final String READ_GAME_COMMAND_MESSAGE =
-            "게임을 다시 시도할지 여부를 입력해주세요. (재시도: " + RETRY_SIGN + ", 종료: " + QUIT_SIGN + " )";
+            "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     public String readBridgeSize() {
         System.out.println(READ_BRIDGE_SIZE_MESSAGE);
