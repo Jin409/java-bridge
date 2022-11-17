02a81ee64beb6d53190dbc3f7e2d605aef81637d
Jin409 <sualng123@gmail.com>
2022-11-18 00:18:23 +0900

    feat(InputGameCommandValidator, ErrorStatus): 재시작 입력값 유효성 검사 기능 구현
    - R 혹은 Q 가 아닌 경우 예외처리

---
+++ src/main/java/bridge/validator/InputGameCommandValidator.java
@@ -0,0 +1,19 @@
+package bridge.validator;
+
+import static bridge.constant.BridgeGameConstant.QUIT_SIGN;
+import static bridge.constant.BridgeGameConstant.RETRY_SIGN;
+import static bridge.validator.ErrorStatus.INVALID_GAME_COMMAND_SIGN_ERROR;
+
+import java.util.Objects;
+
+public class InputGameCommandValidator {
+    public static void validate(String gameCommand) {
+        if (!hasValidGameCommandSign(gameCommand)) {
+            throw new IllegalArgumentException(INVALID_GAME_COMMAND_SIGN_ERROR.getMessage());
+        }
+    }
+
+    private static boolean hasValidGameCommandSign(String gameCommand) {
+        return !Objects.equals(gameCommand, RETRY_SIGN) && !Objects.equals(gameCommand, QUIT_SIGN);
+    }
+}
