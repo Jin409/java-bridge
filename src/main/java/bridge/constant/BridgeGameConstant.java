db18934f5b5bd30ea2effc7dd759f7bbe9ed9ee4
Jin409 <sualng123@gmail.com>
2022-11-17 23:03:16 +0900

    feat(BridgeSizeValidator, ErrorStatus): 다리의 길이 입력값 유효성 검사 기능 구현
    - 정수가 아닌 경우 예외처리
    - 3 이상 20 이하가 아닌 경우 예외처리

--- src/main/java/bridge/constant/BridgeGameConstant.java
+++ src/main/java/bridge/constant/BridgeGameConstant.java
@@ -5,4 +5,6 @@ public class BridgeGameConstant {
     public static final String DOWN_SIGN = "D";
     public static final String RETRY_SIGN = "R";
     public static final String QUIT_SIGN = "Q";
+    public static final int VALID_MIN_RANGE = 3;
+    public static final int VALID_MAX_RANGE = 20;
 }
