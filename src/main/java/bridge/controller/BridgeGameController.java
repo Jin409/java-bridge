73d69255213f3db4851b2e840800513b14467053
Jin409 <sualng123@gmail.com>
2022-11-19 00:33:27 +0900

    feat(BridgeGameController): 게임 흐름 중 게임 시작 문구를 출력하는 기능 구현

---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,20 @@
+package bridge.controller;
+
+import bridge.model.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class BridgeGameController {
+    private BridgeGame bridgeGame;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BridgeGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        outputView.printStartMessage();
+    }
+}
---
+++ src/main/java/bridge/view/OutputView.java
@@ -0,0 +1,15 @@
+package bridge.view;
+
+public class OutputView {
+    private static final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+
+    public void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+
+    public void printMap() {
+    }
+
+    public void printResult() {
+    }
+}
